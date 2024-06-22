0b0c8dc6042c3d73ee3d1b0bad7da084142ebbac
Pablo2vbng <pablo@cvtools.es>
2024-06-22 23:02:21 +0200

    primer commit

---
+++ Proyectos/Frikiweb 2 Demon Slayer/script.js
@@ -0,0 +1,42 @@
+console.log("Hola Mundo");
+
+
+ScrollReveal().reveal('.Person2',    {
+    delay: 100,
+    duration: 500,
+    reset: false,
+    interval: 100
+});
+
+ScrollReveal().reveal('.PersonPrin',    {
+    delay: 100,
+    duration: 500,
+    reset: false,
+    interval: 100
+});
+
+ScrollReveal().reveal('.celda',    {
+    delay: 100,
+    duration: 500,
+    reset: false,
+    interval: 100
+});
+
+ScrollReveal().reveal('.Pelicula',    {
+    delay: 100,
+    duration: 500,
+    reset: false,
+    interval: 100
+});
+
+ScrollReveal().reveal('.celdaanime',    {
+    delay: 100,
+    duration: 500,
+    reset: false,
+    interval: 100
+});
+
+
+
+
+
